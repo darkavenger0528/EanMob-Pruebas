a0acf1c7dabc5fafaaa3736ec49e89b44ff34b2d
Justin Moreno <148641847+darkavenger0528@users.noreply.github.com>
2026-03-10 20:57:11 -0500

    Refactor email config to use env module

--- ean-mob/services/users-service/src/config/email.config.js
+++ ean-mob/services/users-service/src/config/email.config.js
@@ -1,8 +1,9 @@
-require("dotenv").config();
+const env = require("./env");
 
 module.exports = {
-  from: process.env.SMTP_FROM || "no-reply@universidadean.edu.co",
-  otpLength: Number(process.env.OTP_LENGTH) || 6,
-  otpExpiryMinutes: Number(process.env.OTP_EXPIRY_MINUTES) || 10,
-  brevoApiKey: process.env.BREVO_API_KEY,
+  from:             env.SMTP_FROM,
+  otpLength:        Number(env.OTP_LENGTH),
+  otpExpiryMinutes: Number(env.OTP_EXPIRY_MINUTES),
+  brevoApiKey:      env.BREVO_API_KEY,
 };
+
