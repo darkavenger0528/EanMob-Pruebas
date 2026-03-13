e74672fe94928229a571916460da6aae061176e6
Justin Moreno <148641847+darkavenger0528@users.noreply.github.com>
2026-03-13 12:37:38 -0500

    Change server listen address to '0.0.0.0'

--- ean-mob/services/users-service/src/server.js
+++ ean-mob/services/users-service/src/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const env = require("./config/env");
 
-app.listen(Number(env.PORT), () => {
-  console.log(`Users service running on http://localhost:${env.PORT}`);
+app.listen(Number(env.PORT), "0.0.0.0", () => {
+  console.log(`Users service running on port ${env.PORT}`);
 });
